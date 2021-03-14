3afd32ee709e47d32aac21cc503daa57331c3df9
SekretTikhon <sekret.tikhon@gmail.com>
2021-03-14 13:45:21 +0300

    added logs

--- scripts/WebServer.rb
+++ scripts/WebServer.rb
@@ -5,12 +5,11 @@
 #require 'pathname'
 #require 'logger'
 
-
 configure do
   enable :sessions
   register Config
 
-  #set :root implicity
+  #:root sets implicity
   set :logger, Logger.new(STDOUT)
   set :views, "#{settings.root}/views"
   set :no_auth_neededs, ['/login']
@@ -35,7 +34,8 @@ get '/login' do
   erb :login if are_login
 
   params = request.env['rack.request.query_hash']
-  code = params["code"]
+  code = params['code']
+  logger.debug "'get /login' with code '#{code}'"
   login_with_code code
 
   erb :login
@@ -43,25 +43,26 @@ end
 
 get '/fs' do
   session['cur_path'] = session['homepath']
+  logger.debug "'get /fs' to homepath '#{session['homepath']}'"
   redirect_to_cur_path
 end
 
 get '/fs*' do
-  puts Settings.param
-  #logger.info 'its logger message'
   path = params['splat'][0]
+  logger.debug "'get /fs*' with path '#{path}'"
 
   if !File.exist?(path)
-    puts 'path ' + path + ' not exists!' #todo print message
+    logger.debug "'get /fs*' with path '#{path}' is not exists!" #todo print error message
     redirect_to_cur_path
   end
   if File.file?(path)
-    #puts path + ' is file.'
+    logger.debug "'get /fs*' with path '#{path}' it's file"
     send_file(path, :disposition => 'attachment', :type => 'Application/octet-stream')
     redirect_to_cur_path
   end
   if !File.directory?(path)
-    puts path + ' is not directory.' #todo print message
+    logger.debug "'get /fs*' with path '#{path}' is not directory!"
+    puts path + ' is not directory.' #todo print error message
     redirect_to_cur_path
   end
 
@@ -69,7 +70,10 @@ get '/fs*' do
   path = path + '/' if path[-1] != '/'
 
   #перенаправление если нет прав
-  redirect_to_cur_path if !system("sudo -u #{session['user']} [ -r #{path} ]") #todo print error message
+  if !system("sudo -u #{session['user']} [ -r #{path} ]") #todo print error message
+    logger.debug "'get /fs*' with path '#{path}' permission denied!"
+    redirect_to_cur_path
+  end
   
   #переменные для шаблона
   @directoryes, @files = parse_dirs_files(path)
@@ -81,6 +85,7 @@ end
 
 get '/download' do
   filenames = params['filenames'].split(',')
+  logger.debug "'get /download' with filenames: '#{filenames}'"
 
   if filenames
     download_files(filenames)
@@ -90,30 +95,36 @@ get '/download' do
 end
 
 get '/upload' do
+  logger.debug "'get /upload'"
   @default_path = session['cur_path']
 
   erb :upload
 end
 
 post '/upload' do
+  logger.debug "'post /upload''"
   upload_file
 
   redirect_to_cur_path
 end
 
 get '/cat' do
+  logger.debug "meow!!!"
   send_file 'cat.jpeg'
 end
 
 get '/error' do
+  logger.debug "'get /error''"
   halt 401, 'go away!'
 end
 
 def redirect_to_cur_path 
+  logger.debug "'redirect_to_cur_path' with cur_path '#{session['cur_path']}'"
   redirect to ('/fs' + session['cur_path'])
 end
 
 def parse_dirs_files (path)
+  logger.debug "'parse_dirs_files' with path '#{path}'"
   directoryes = []
   files = []
 
@@ -132,6 +143,7 @@ def parse_dirs_files (path)
 end
 
 def get_fullpath_map (path)
+  logger.debug "'get_fullpath_map' with path '#{path}'"
   map = []
   ref = '/fs'
   path.split('/').each do |elem|
@@ -144,7 +156,7 @@ def get_fullpath_map (path)
 end
 
 def download_files (filenames)
-  puts filenames
+  logger.debug "'download_files' with filenames '#{filenames}'"
   dir_path = File.dirname(filenames[0])
   filenames = filenames.map {|filename| File.basename(filename)}
 
@@ -158,6 +170,7 @@ def download_files (filenames)
 end
 
 def upload_file
+  logger.debug "'upload_file'"
   tempfile = params['file'][:tempfile]
   filename = params['file'][:filename]
   path = params['path']
@@ -169,6 +182,7 @@ def are_login
 end
 
 def db_execute comand
+  logger.debug "'db_execute' with comand '#{comand}'"
   db = SQLite3::Database.open Settings.db.path
   res = db.execute comand
   db.close if db
@@ -176,6 +190,8 @@ def db_execute comand
 end
 
 def login_with_code code
+  logger.debug "'login_with_code' with code '#{code}'"
+
   #request to db
   row = db_execute "
     SELECT #{settings.db_codes_columns.join(', ')}
@@ -184,7 +200,7 @@ def login_with_code code
 
   #check that code found
   if row == []
-    @error = "the code not found"
+    @error = "This code not found!"
     return
   end
   row = row[-1] #becase res is array of rows
@@ -192,7 +208,7 @@ def login_with_code code
   #check that the code already never used
   already_used = row[settings.db_codes_columns.index('already_used')]
   if already_used != 0
-    @error = "the code already used"
+    @error = "This code already used!"
     return
   end
 
@@ -200,7 +216,7 @@ def login_with_code code
   valid_until = row[settings.db_codes_columns.index('valid_until')]
   now_timestamp = `date +%s`.to_i
   if valid_until <= now_timestamp
-    @error = "the code expired"
+    @error = "This code expired!"
     return
   end
 
@@ -218,6 +234,3 @@ def login_with_code code
 
 end
 
-def check_login
-  redirect to ('/') if !are_login
-end
